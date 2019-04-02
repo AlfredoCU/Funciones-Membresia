e7f4e7b6c5e3726ef1780868333fde3667dca722
AlfredoCU <alfredo_11_cu@hotmail.com>
2019-04-02 13:32:52 -0600

    Se a creado una biblioteca fuzzy para funciones de membresia.

---
+++ FuncionesMembresia/BiblioFuzzy.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncionesMembresia {
+    class BiblioFuzzy {
+        // Atributo privado de valor.
+        private double val;
+        
+        //Constructor vacio.
+        public BiblioFuzzy() { }
+
+        // Métodos de Membresía.
+        public double TrapecioAbiertoDer(double u, double a, double b) {
+            if (a <= u && u <= b) {
+                val = (u - a) / (b - a);
+            }
+            else if (u > b) {
+                val = 1.0;
+            }
+            else {
+                val = 0.0; // (u < a)
+            }
+            return val;
+        }
+
+        public double TrapecioAbiertoIzq(double u, double a, double b) {
+            if (a <= u && u <= b) {
+                val = (b - u) / (b - a);
+            }
+            else if (u < a) {
+                val = 1.0;
+            }
+            else {
+                val = 0.0; // (u > b)
+            }
+            return val;
+        }
+
+        public double Triangular(double u, double a, double b, double c) {
+            if (a <= u && u < b) {
+                val = (u - a) / (b - a);
+            }
+            else if (b <= u && u <= c) {
+                val = (c - u) / (c - b);
+            }
+            else {
+                val = 0.0; // (u < a || u > c)
+            }
+            return val;
+        }
+
+        public double Trapezoidal(double u, double a, double b, double c, double d) {
+            if (a <= u && u < b) {
+                val = (u - a) / (b - a);
+            }
+            else if (c < u && u <= d) {
+                val = (d - u) / (d - c);
+            }
+            else if (b <= u && u <= c) {
+                val = 1.0;
+            }
+            else {
+                val = 0.0; // (u < a || u > d)
+            }
+            return val;
+        }
+
+        public double Curva_S(double u, double a, double b) {
+            if (a <= u && u <= b) {
+                val = (1 + Math.Cos(((u - b) / (b - a)) * Math.PI)) / 2.0;
+            }
+            else if (u > b) {
+                val = 1.0;
+            }
+            else {
+                val = 0.0; // (u < a)
+            }
+            return val;
+        }
+
+        public double Curva_Z(double u, double a, double b) {
+            if (a <= u && u <= b) {
+                val = (1 + Math.Cos(((u - a) / (b - a)) * Math.PI)) / 2.0;
+            }
+            else if (u < a) {
+                val = 1.0;
+            }
+            else {
+                val = 0.0; // (u > b)
+            }
+            return val;
+        }
+
+        public double TriangularSuave(double u, double a, double b, double c) {
+            if (a <= u && u < b) {
+                val = (1 + Math.Cos(((u - b) / (b - a)) * Math.PI)) / 2.0;
+            }
+            else if (b <= u && u <= c) {
+                val = (1 + Math.Cos(((b - u) / (c - b)) * Math.PI)) / 2.0;
+            }
+            else {
+                val = 0.0; // (u < a || u > c)
+            }
+            return val;
+        }
+
+        public double TrapezoidalSuave(double u, double a, double b, double c, double d) {
+            if (a <= u && u < b) {
+                val = (1 + Math.Cos(((u - b) / (b - a)) * Math.PI)) / 2.0;
+            }
+            else if (c < u && u <= d) {
+                val = (1 + Math.Cos(((c - u) / (d - c)) * Math.PI)) / 2.0;
+            }
+            else if (b <= u && u <= c) {
+                val = 1.0;
+            }
+            else {
+                val = 0.0; // (u < a || u > d)
+            }
+            return val;
+        }
+
+        // Operadores Lógicos Fuzzy.
+        public double CompAND(double ma_u, double mb_u) {
+            return Math.Min(ma_u, mb_u);
+        }
+
+        public double CompOR(double ma_u, double mb_u) {
+            return Math.Max(ma_u, mb_u);
+        }
+
+        public double Niega(double ma_u) {
+            return 1.0 - ma_u;
+        }
+
+        // Implicación Fuzzy.
+        public double ImplicaZadeh(double ma_x, double mb_y) {
+            return Math.Max(Math.Min(ma_x, mb_y), Niega(ma_x));
+        }
+
+        public double ImplicaMamdani(double ma_x, double mb_y) {
+            return Math.Min(ma_x, mb_y);
+        }
+
+        public double ImplicaGodel(double ma_x, double mb_y) {
+            return val = (ma_x <= mb_y) ? val = 1 : val = mb_y;
+        }
+    }
+}
---
+++ FuncionesMembresia/Menu.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncionesMembresia {
+    class Menu {
+        // Constructor.
+        public Menu() {
+            Interfaz();
+        }
+
+        // Interfaz de la consola.
+        public void Interfaz() {
+            BiblioFuzzy b = new BiblioFuzzy();
+            int u = 0, li = 0, ld = 30, lsi = 10, lsd = 20, lc = 15;
+
+            Console.Title = "BiblioFuzzy";
+            Console.WriteLine("********** Trapecio Abierto por la Izquierda **********");
+            for (u = li; u < ld; u++) {
+                Console.WriteLine("Para u=" + u + ", TAI(u): " + b.TrapecioAbiertoIzq(u, li + 10, ld - 10));
+            }
+
+            Console.WriteLine("\n********** Trapecio Abierto por la Derecha **********");
+            for (u = li; u < ld; u++) {
+                Console.WriteLine("Para u=" + u + ", TAD(u): " + b.TrapecioAbiertoDer(u, li + 10, ld - 10));
+            }
+
+            Console.WriteLine("\n********** Triangular **********");
+            for (u = li; u < ld; u++) {
+                Console.WriteLine("Para u=" + u + ", Triang(u): " + b.Triangular(u, li + 10, lc, ld - 10));
+            }
+
+            Console.WriteLine("\n********** Trapezoidal **********");
+            for (u = li; u < ld; u++) {
+                Console.WriteLine("Para u=" + u + ", Trap(u): " + b.Trapezoidal(u, li + 5, lsi, lsd, ld - 5));
+            }
+
+            Console.WriteLine("\n********** Curva Z **********");
+            for (u = li; u < ld; u++) {
+                Console.WriteLine("Para u=" + u + ", CZ(u): " + b.Curva_Z(u, li + 5, ld - 5));
+            }
+
+            Console.WriteLine("\n********** Curva S **********");
+            for (u = li; u < ld; u++) {
+                Console.WriteLine("Para u=" + u + ", CS(u): " + b.Curva_S(u, li + 5, ld - 5));
+            }
+
+            Console.WriteLine("\n********** Triangular Suave **********");
+            for (u = li; u < ld; u++) {
+                Console.WriteLine("Para u=" + u + ", TS(u): " + b.TriangularSuave(u, li + 5, lc, ld - 5));
+            }
+
+            Console.WriteLine("\n********** Trapezoidal Suave **********");
+            for (u = li; u < ld; u++) {
+                Console.WriteLine("Para u=" + u + ", TrapS(u): " + b.TrapezoidalSuave(u, li + 5, lsi, lsd, ld - 5));
+            }
+            PresioneSalir();
+        }
+
+        // Mensaje final para terminar la ejecución.
+        public void PresioneSalir() {
+            Console.Write("\nPresione para salir...");
+            Console.ReadKey();
+        }
+    }
+}
